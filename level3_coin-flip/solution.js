5bbc236e0c646be6b68e00c8d0b21519ceb4bdf7
mitchellnel <mitchellmclin@gmail.com>
2022-05-25 11:20:23 -0700

    Update to level 3 solution method

--- level3_coin-flip/solution.js
+++ level3_coin-flip/solution.js
@@ -1,6 +1,9 @@
 // deploy PsychicCoinFlip contract via Remix IDE
 // call psychicCoinFlip() method 10 times
 
+await contract.consecutiveWins();
+// >> returns 10
+
 // submit
 
 // I manually made these calls, I'll probably setup a HardHat env and try to
